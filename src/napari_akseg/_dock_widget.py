b9576f32de0c26a0c69766bcc851e766b09da888
Piers Turner <piers.turner@physics.ox.ac.uk>
2022-02-15 09:16:10 +0000

    Removed update metadata slider function

--- src/napari_akseg/_dock_widget.py
+++ src/napari_akseg/_dock_widget.py
@@ -1876,7 +1876,7 @@ class AKSEG(QWidget):
 
             self.viewer.text_overlay.text = file_name
 
-            update_akmetadata(self, metadata)
+            # update_akmetadata(self, metadata)
 
         except:
             pass
